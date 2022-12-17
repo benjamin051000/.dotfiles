9a1cbe5f181080e6a7c9741bbb2074c85342a1c7
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2022-12-17 13:35:45 -0500

    Auto center vert on pg up/dn

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,15 @@
 require("benjamin.set")
 require("benjamin.telescope")
 
+
+local map = vim.api.nvim_set_keymap
+local opts = {noremap = true, silent = true}
+-- Remap pageup and pagedown to also center screen
+map("n", "<C-d>", "<C-d>zz", opts)
+map("n", "<C-u>", "<C-u>zz", opts)
+-- Center screen (horizontal and vertical) when using n/N
+map("n", "n", "nzzzv", opts)
+map("n", "N", "Nzzzv", opts)
+
+vim.opt.undofile = true
+
