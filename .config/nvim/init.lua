0c6b39ae39008fb768feb1882297bce96a8c40ba
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2023-12-23 09:24:10 -0500

    moved telescope to after folder, where it belongs.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,8 @@
 -- Helpful resource: https://vonheikemen.github.io/devlog/tools/configuring-neovim-using-lua/
 require("benjamin.set")
-require("benjamin.telescope")
-
 
 local map = vim.api.nvim_set_keymap
-local opts = {noremap = true, silent = true}
+local opts = { noremap = true, silent = true }
 -- Remap pageup and pagedown to also center screen
 map("n", "<C-d>", "<C-d>zz", opts)
 map("n", "<C-u>", "<C-u>zz", opts)
@@ -13,4 +11,3 @@ map("n", "n", "nzzzv", opts)
 map("n", "N", "Nzzzv", opts)
 
 vim.opt.undofile = true
-
