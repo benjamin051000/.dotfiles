c26163d42adcdbb94a766c014ab91207f78fde59
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-04-10 07:10:57 -0400

    various lsp updates

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -12,102 +12,102 @@ vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
 
 -- Shamelessly stolen from https://github.com/AlphaKeks/home/blob/master/.config/nvim/after/plugin/lsp.lua#L94-102
 local function format_on_save(bufnr)
-    vim.api.nvim_create_autocmd("BufWritePre", {
-        group = format_group,
-        buffer = bufnr,
-        callback = function()
-            vim.lsp.buf.format()
-        end,
-    })
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		group = format_group,
+		buffer = bufnr,
+		callback = function()
+			vim.lsp.buf.format()
+		end,
+	})
 end
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-    -- Enable symbol completion triggered by <C-x><C-o>
-    -- Code completion: <C-x><C-o>
-    vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	-- Enable symbol completion triggered by <C-x><C-o>
+	-- Code completion: <C-x><C-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap = true, silent = false, buffer = bufnr }
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = false, buffer = bufnr }
 
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)  -- go to Declaration
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)   -- go to Definition
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set("n", "<C-space>", vim.lsp.buf.hover, bufopts) -- hover gives details about the symbol the cursor is on
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts) -- go to Declaration
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts) -- go to Definition
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<C-space>", vim.lsp.buf.hover, bufopts) -- hover gives details about the symbol the cursor is on
 
-    -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
 
-    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-    vim.keymap.set("n", "<space>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, bufopts)
-    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-    vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-    -- TODO BUG sometimes this doesn't show up, like for stylua (which doesn't have an lsp here...)
-    vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
+	vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set("n", "<space>wl", function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	-- TODO BUG sometimes this doesn't show up, like for stylua (which doesn't have an lsp here...)
+	vim.keymap.set("n", "<space>f", vim.lsp.buf.format, bufopts)
 
-    format_on_save(bufnr)
+	-- format_on_save(bufnr)
 end
 
 local lsp_flags = {
-    -- This is the default in Nvim 0.7+
-    debounce_text_changes = 150,
+	-- This is the default in Nvim 0.7+
+	debounce_text_changes = 150,
 }
 
 require("lspconfig").pyright.setup({
-    capabilities = default_capabilities,
-    on_attach = on_attach,
-    flags = lsp_flags,
+	capabilities = default_capabilities,
+	on_attach = on_attach,
+	flags = lsp_flags,
 })
 
-require("lspconfig")["tsserver"].setup({
-    capabilities = default_capabilities,
-    on_attach = on_attach,
-    flags = lsp_flags,
-})
+-- require("lspconfig")["tsserver"].setup({
+--     capabilities = default_capabilities,
+--     on_attach = on_attach,
+--     flags = lsp_flags,
+-- })
 
 local rt = require("rust-tools")
 rt.setup({
-    server = {
-        on_attach = function(client, bufnr)
-            -- Run the normal on_attach function for all LSP stuffs
-            on_attach(client, bufnr)
+	server = {
+		on_attach = function(client, bufnr)
+			-- Run the normal on_attach function for all LSP stuffs
+			on_attach(client, bufnr)
 
-            -- Hover actions
-            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-            -- Code action groups
-            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-        end,
+			-- Hover actions
+			vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+			-- Code action groups
+			vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+		end,
 
-        capabilities = default_capabilities,
+		capabilities = default_capabilities,
 
-        settings = {
-            -- See settings here: https://github.com/rust-lang/rust-analyzer/blob/master/docs/user/generated_config.adoc
-            -- and here: https://hw0lff.github.io/rust-analyzer-docs/2022-07-04/index.html#rust-analyzer_cargo_buildscripts_enable
-            ["rust-analyzer"] = {
-                -- For now, let's just stick with the defaults.
+		settings = {
+			-- See settings here: https://github.com/rust-lang/rust-analyzer/blob/master/docs/user/generated_config.adoc
+			-- and here: https://hw0lff.github.io/rust-analyzer-docs/2022-07-04/index.html#rust-analyzer_cargo_buildscripts_enable
+			["rust-analyzer"] = {
+				-- For now, let's just stick with the defaults.
 
-                -- imports = {
-                -- 	granularity = {
-                -- 		group = "module",
-                -- 	},
-                -- 	prefix = "self",
-                -- },
-            },
-        }, -- end of settings
-    },     -- end of server
+				-- imports = {
+				-- 	granularity = {
+				-- 		group = "module",
+				-- 	},
+				-- 	prefix = "self",
+				-- },
+			},
+		}, -- end of settings
+	}, -- end of server
 })
 
 -- c/c++
 require("lspconfig").clangd.setup({
-    capabilities = default_capabilities,
-    on_attach = on_attach,
-    flags = lsp_flags,
+	capabilities = default_capabilities,
+	on_attach = on_attach,
+	flags = lsp_flags,
 })
 
 -- require 'lspconfig'.shellcheck.setup{
@@ -117,16 +117,16 @@ require("lspconfig").clangd.setup({
 -- }
 
 -- lsp lines
--- require("lsp_lines").setup()
+require("lsp_lines").setup()
 
 vim.diagnostic.config({
-    virtual_text = false,
+	virtual_text = false,
 })
 
 require("lspconfig").lua_ls.setup({
-    capabilities = default_capabilities,
-    on_attach = on_attach,
-    flags = lsp_flags,
+	capabilities = default_capabilities,
+	on_attach = on_attach,
+	flags = lsp_flags,
 })
 
--- vim.keymap.set("", "<leader>el", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+vim.keymap.set("", "<leader>el", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
