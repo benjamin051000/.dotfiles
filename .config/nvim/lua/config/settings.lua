47b89c2ef388020d198b7a1290c1f78c58038fa4
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2025-03-26 07:49:40 -0400

    Settings cleanup

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -1,6 +1,6 @@
 -- General settings and keymaps
 
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
@@ -11,21 +11,18 @@ vim.opt.shiftwidth = 4
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
+vim.opt.autoindent = true
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
-
--- Remap Ctrl-C to Esc, which triggers events that LSPs need to update.
--- Also, allows use of 3iabc<Esc> and 3iabc<C-c> to repeat inserts
-vim.cmd("inoremap <C-c> <Esc>")
 
 -- set leader key to spacebar (instead of \)
 vim.g.mapleader = " "
 
 -- fun one
-vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
+-- vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
 
-local map = vim.api.nvim_set_keymap
+local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 -- Remap pageup and pagedown to also center screen
 map("n", "<C-d>", "<C-d>zz", opts)
@@ -33,5 +30,9 @@ map("n", "<C-u>", "<C-u>zz", opts)
 -- Center screen (horizontal and vertical) when using n/N
 map("n", "n", "nzzzv", opts)
 map("n", "N", "Nzzzv", opts)
+
+-- Remap Ctrl-C to Esc, which triggers events that LSPs need to update.
+-- Also, allows use of 3iabc<Esc> and 3iabc<C-c> to repeat inserts
+map("i", "<C-c>", "<Esc>")
 
 vim.opt.undofile = true
